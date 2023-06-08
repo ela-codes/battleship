40182535ea279092b1b71892c8c4530480bda8a9
ateodocio <teodocio.am@gmail.com>
2023-06-08 18:54:02 -0500

    Report whether all ships have sunk

--- src/factories/factoryTests/gameboardFactory.test.js
+++ src/factories/factoryTests/gameboardFactory.test.js
@@ -8,20 +8,92 @@ const correctGameboard = {
     submarine: new Ship('submarine', 3),
     destroyer: new Ship('destroyer', 2),
     board: [
-        ["destroyer", "destroyer", null, null, "cruiser", null, null, null, null, null ],
-        [null, null, null, null, "cruiser", null, "submarine", "submarine", "submarine", null ],
-        [null, null, null, null, "cruiser", null, null, null, null, null ],
-        [null, null, null, null, null, null, null, null, null, null ],
-        [null, null, null, "carrier", null, null, null, null, null, null ],
-        [null, null, null, "carrier", null, null, null, null, null, null ],
-        [null, null, null, "carrier", null, null, null, null, null, null ],
-        [null, null, null, "carrier", null, null, null, null, null, null ],
-        [null, null, null, "carrier", null, null, null, null, null, null ],
-        [null, null, "battleship", "battleship", "battleship", "battleship", null, null, null, null ]
-    ]
+        ["destroyer", "destroyer", null, null, "cruiser", null, null, null, null, null],
+        [null, null, null, null, "cruiser", null, "submarine", "submarine", "submarine", null],
+        [null, null, null, null, "cruiser", null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, "carrier", null, null, null, null, null, null],
+        [null, null, null, "carrier", null, null, null, null, null, null],
+        [null, null, null, "carrier", null, null, null, null, null, null],
+        [null, null, null, "carrier", null, null, null, null, null, null],
+        [null, null, null, "carrier", null, null, null, null, null, null],
+        [null, null, "battleship", "battleship", "battleship", "battleship", null, null, null, null]
+    ],
+    missedAttacks: [],
+    successfulAttacks: []
 }
-const battleshipGame = new Gameboard;
 
-test('correctly creates a gameboard obj', () => {
-    expect(battleshipGame).toEqual(correctGameboard)
+
+describe('gameboard creation', () => {
+    const battleshipGame = new Gameboard;
+    test('correctly creates a gameboard obj with ships positioned', () => {
+        expect(battleshipGame).toEqual(correctGameboard)
+    })
+    
+})
+
+describe('gameboard activities', () => {
+    const battleshipGame = new Gameboard;
+    const attack = battleshipGame.receiveAttack(0,1)
+
+    test('attack causes damage to ship', () => {
+        expect(battleshipGame.destroyer.damage).toBe(1)
+    })
+
+    test('successful attack outputs correct notification', () => {
+        expect(attack).toBe("It's a hit!")
+    })
+
+
+    const correctMissedAttacks = [[9,9], [8,7]]
+    const attack1 = battleshipGame.receiveAttack(9, 9)
+    const attack2 = battleshipGame.receiveAttack(8, 7)
+
+    test('stores missed attacks', () => {
+        expect(battleshipGame.missedAttacks).toEqual(correctMissedAttacks)
+    })
+
+    test('missed attack outputs correct notifications', () => {
+        expect(attack2).toBe("It's a miss!")
+    })
+
+    const attack3 = battleshipGame.receiveAttack(0, 10)
+    test('recognizes invalid attack coordinates', () => {
+        expect(attack3).toBe('Invalid attack')
+    })
+
+    const attack4 = battleshipGame.receiveAttack(0, 10)
+    test('recognizes attack coordinates that were used previously', () => {
+        expect(attack4).toBe('Invalid attack')
+    })
+
+    test('not all ships have sunk', () => {
+        expect(battleshipGame.areAllShipsSunk()).toBe(false)
+    })
+})
+
+
+describe('sink all ships on board', () => {
+    const battleshipGame = new Gameboard;
+
+    function getAllValidAttacks() {
+        const allAttacks = []
+        for (let i = 0; i <= 9; i++) {
+            for (let j = 0; j <= 9; j++) {
+                if (correctGameboard.board[i][j] !== null) {
+                    allAttacks.push([i,j])
+                }
+            }
+        } return allAttacks
+    }
+
+    const allAttacks = getAllValidAttacks()
+
+    for (let i of allAttacks) {
+        battleshipGame.receiveAttack(i[0], i[1])
+    }
+
+    test('all ships have sunk', () => {
+        expect(battleshipGame.areAllShipsSunk()).toBe(true)
+    })
 })
--- src/factories/gameboardFactory.js
+++ src/factories/gameboardFactory.js
@@ -8,6 +8,8 @@ class Gameboard {
         this.submarine = this.#createShip('submarine', 3)
         this.destroyer = this.#createShip('destroyer', 2)
         this.board = this.#positionShips(this.#createBoard())
+        this.missedAttacks = []
+        this.successfulAttacks = []
     }
 
     #createBoard() {
@@ -21,13 +23,23 @@ class Gameboard {
         }
         return board
     }
-
+    
 
     #createShip(name, length) {
         const ship = new Ship(name, length)
         return ship
     }
 
+    #getAllShips() {
+        const ships = [
+            this.carrier, 
+            this.battleship, 
+            this.cruiser, 
+            this.submarine, 
+            this.destroyer
+        ]
+        return ships
+    }
 
     #positionShips(gameboard) {
         const board = gameboard
@@ -61,13 +73,8 @@ class Gameboard {
             board[x][y] = shipName
         }
 
-        const ships = [
-            this.carrier, 
-            this.battleship, 
-            this.cruiser, 
-            this.submarine, 
-            this.destroyer
-        ]
+        
+        const ships = this.#getAllShips()
 
         for (let ship of ships) {
             const position = this.#getPositioning(ship.name)
@@ -90,6 +97,78 @@ class Gameboard {
             case "destroyer":
                 return [0, 0, 'east']
         }
+    }
+
+    getBoard() {
+        const boardCopy = [...this.board]
+        return boardCopy
+    }
+
+    getMissedAttacks() {
+        const missedAttackArrCopy = [...this.missedAttacks]
+        return missedAttackArrCopy
+    }
+
+    getSuccessfulAttacks() {
+        const successfulAttackArrCopy = [...this.successfulAttacks]
+        return successfulAttackArrCopy
+    }
+
+    #storeMissedAttack(x, y) {
+        this.missedAttacks.push([x,y])
+    }
+
+    #storeSuccessfulAttack(x, y) {
+        this.successfulAttacks.push([x, y])
+    }
+
+    #isValidAttack(x, y) {
+        // all should be true
+        const uniqueMissedAttack = !this.getMissedAttacks().includes([x, y])
+        const uniqueSuccessfulAttack = !this.getSuccessfulAttacks().includes([x, y]) 
+        const validCoordinates = (x >= 0 && x <= 9) && (y >= 0 && y <= 9)
+
+        return uniqueMissedAttack && uniqueSuccessfulAttack && validCoordinates
+    }
+
+    receiveAttack(x, y) {
+        // check if there is a ship at [x, y] coordinates
+        // if yes, apply damage to this.ship & record successful attack
+        // if not, record the coordinates of the missed attack
+
+        if (this.#isValidAttack(x, y)) {
+            const board = this.getBoard()
+            const ships = {
+                'carrier': this.carrier, 
+                'battleship': this.battleship, 
+                'cruiser': this.cruiser, 
+                'submarine': this.submarine, 
+                'destroyer': this.destroyer
+            }
+
+            if (board[x][y] !== null) {
+                ships[board[x][y]].hit()
+                this.#storeSuccessfulAttack(x, y)
+                return "It's a hit!"
+            } else {
+                this.#storeMissedAttack(x, y)
+                return "It's a miss!"
+            }
+
+        } else return 'Invalid attack'
+    }
+
+    areAllShipsSunk() {
+        if (this.getSuccessfulAttacks().length >= 17) {
+            const ships = this.#getAllShips()
+            const status = ships.every(ship => ship.sunk === true)
+
+            if (status) console.log('All ships have sunk!!!')
+
+            return status
+            
+        } return false
+
     }
 }
 
