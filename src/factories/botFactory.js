7b8c3a6c231bc5935e6b8d0cd038f808b5e120eb
ateodocio <teodocio.am@gmail.com>
2023-06-16 06:52:47 -0500

    Set up html and main script files

--- src/factories/botFactory.js
+++ src/factories/botFactory.js
@@ -1,8 +1,15 @@
 import Gameboard from './gameboardFactory'
 
 class Bot {
+    #successfulAttack;
+    #previousAttack;
+    #possibleSmartMoves;
+
     constructor() {
         this.board = this.#createBoard()
+        this.#previousAttack = null
+        this.#successfulAttack = false
+        this.#possibleSmartMoves = [[0, 1], [0, -1], [-1, 0], [1, 0]]
     }
 
     #createBoard() {
@@ -13,7 +20,9 @@ class Bot {
     getCoordinates(enemyGameboard) {
         // pick a random spot within the board
         // should be a unique coordinate every time
-        // if previous coordinate was a hit, choose a coordinate +-1 x , +-1 y (TO DO)
+        // if previous coordinate was a hit, choose an adjacent coordinate
+        // improvement -- generate next coordinate based on available empty slots instead of random x/y coords
+        
         function getRandomNum(min, max) {
             const num = Math.random() * (max - min) + min;
             return Math.floor(num)
@@ -21,6 +30,17 @@ class Bot {
 
         function isEmptyPosition(x, y, enemyBoard) {
             return enemyBoard[x][y] === null
+        }
+
+
+        if (this.#successfulAttack) {
+            if (this.#possibleSmartMoves.length > 0) {
+                const positionOffset = this.#possibleSmartMoves.pop()
+                let xCoord = this.#previousAttack[0] + positionOffset[0]
+                let yCoord = this.#previousAttack[1] + positionOffset[1]
+
+                return [xCoord, yCoord]
+            }
         }
 
         let xCoord = getRandomNum(0, 9)
@@ -38,7 +58,17 @@ class Bot {
     attackEnemy(coordinatesArr, enemyBoard) {
         const [x, y] = [...coordinatesArr] 
         const attackFeedback = enemyBoard.receiveAttack(x, y)
+        if (attackFeedback === "It's a hit!") {
+            this.#storePreviousAttack(coordinatesArr, true)
+        } else {
+            this.#storePreviousAttack(coordinatesArr, false)
+        }
         return attackFeedback
+    }
+
+    #storePreviousAttack(coordinatesArr, enemyWasHit) {
+        this.#previousAttack = coordinatesArr
+        this.#successfulAttack = enemyWasHit
     }
 
 }
